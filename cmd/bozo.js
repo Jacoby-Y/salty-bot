a1e8e19c6820031ad6b6a9726b5f8a0da78b81b7
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-08-17 13:13:42 -0500

    Added randomness to /bozo

--- cmd/bozo.js
+++ cmd/bozo.js
@@ -7,13 +7,15 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bozo')
-		.setDescription('Bozo moment'),
+		.setDescription("Will randomly state whether you're a bozo or not"),
 	async execute(interaction) {
         // Snatched from a demo
+        const title = (Math.random() < 0.5 ? "You're *not* a bozo" : "You *are* a bozo");
+
         let embed = new EmbedBuilder()
             .setColor(0x000000)
-            .setTitle("You're a *bozo*")
-            .setTimestamp()
+            .setTitle(title)
+            // .setTimestamp()
             // .setFooter({ text: '/help for more commands' });
 
         await interaction.reply({ embeds: [embed] });
